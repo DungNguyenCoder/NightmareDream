b52e616d718bbbfd0b7a140d8a57f4934d73869c
DungNguyenCoder <nm.dung.it@gmail.com>
2026-01-17 14:29:09 +0700

    Update: Audio

--- Nightmare Dream/Assets/Scripts/UI/SettingPanel.cs
+++ Nightmare Dream/Assets/Scripts/UI/SettingPanel.cs
@@ -1,7 +1,54 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingPanel : Panel
 {
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey(GameConfig.MUSIC_VOLUME_KEY))
+            LoadMusicVolume();
+        else
+            SetMusicVolume();
+
+        if (PlayerPrefs.HasKey(GameConfig.SFX_VOLUME_KEY))
+            LoadSFXVolume();
+        else
+            SetSFXVolume();
+    }
+    public void SetMusicVolume()
+    {
+        Debug.Log("Set Music");
+        float volume = musicSlider.value;
+        myMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(GameConfig.MUSIC_VOLUME_KEY, volume);
+        PlayerPrefs.Save();
+    }
+    public void SetSFXVolume()
+    {
+        Debug.Log("Set SFX");
+        float volume = sfxSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(GameConfig.SFX_VOLUME_KEY, volume);
+        PlayerPrefs.Save();
+    }
+    private void LoadMusicVolume()
+    {
+        Debug.Log("Load Music");
+        float volume = PlayerPrefs.GetFloat(GameConfig.MUSIC_VOLUME_KEY);
+        musicSlider.value = volume;
+    }
+
+    private void LoadSFXVolume()
+    {
+        Debug.Log("Load SFX");
+        float volume = PlayerPrefs.GetFloat(GameConfig.SFX_VOLUME_KEY);
+        sfxSlider.value = volume;
+    }
     public void OnClickCreditButton()
     {
         Application.OpenURL(GameConfig.URL);
--- Nightmare Dream/Assets/Scripts/Utils/GameConfig.cs
+++ Nightmare Dream/Assets/Scripts/Utils/GameConfig.cs
@@ -6,5 +6,7 @@ public class GameConfig
     public static string PANEL_INSTRUCTION = "InstuctionPanel";
     public static string PANEL_SETTING = "SettingPanel";
     public static string PANEL_PAUSE = "PausePanel";
+    public static string MUSIC_VOLUME_KEY = "MusicVolume";
+    public static string SFX_VOLUME_KEY = "SFXVolume";
     public static string URL = "https://github.com/DungNguyenCoder/NightmareDream";
 }
